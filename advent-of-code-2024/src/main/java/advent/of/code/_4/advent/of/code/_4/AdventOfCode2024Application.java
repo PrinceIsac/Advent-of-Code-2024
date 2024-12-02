b9474c3cedad473902be18b82f1afefb1fa90aaa
Prince Luhanga <prince@synthesis.co.za>
2024-12-02 13:52:28 +0200

    Finished Day 1 Exercises.

--- advent-of-code-2024/src/main/java/advent/of/code/_4/advent/of/code/_4/AdventOfCode2024Application.java
+++ advent-of-code-2024/src/main/java/advent/of/code/_4/advent/of/code/_4/AdventOfCode2024Application.java
@@ -3,7 +3,6 @@ package advent.of.code._4.advent.of.code._4;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -11,7 +10,12 @@ import java.util.List;
 public class AdventOfCode2024Application {
 
 	public static void main(String[] args) {
+		List<Integer> list1 =  Arrays.asList(1,2,3,4,5); //4 + 4 + 5
+		List<Integer> list2 =  Arrays.asList(2,2,0,4,5);
 		SpringApplication.run(AdventOfCode2024Application.class, args);
-		//Day One
-		System.out.println(Day1.calculateDistanceBetweenLists(InputClass.dayOneInput().getFirst(), InputClass.dayOneInput().getLast()));
+		//Solution One
+		System.out.println(Day1.solution1(InputClass.dayOneInput().getFirst(), InputClass.dayOneInput().getLast()));
+
+		//Solution Two
+		System.out.println(Day1.solution2(InputClass.dayOneInput().getFirst(), InputClass.dayOneInput().getLast()));
 	}}
--- advent-of-code-2024/src/main/java/advent/of/code/_4/advent/of/code/_4/Day1.java
+++ advent-of-code-2024/src/main/java/advent/of/code/_4/advent/of/code/_4/Day1.java
@@ -1,10 +1,14 @@
 package advent.of.code._4.advent.of.code._4;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
 
 public class Day1 {
-    public static int calculateDistanceBetweenLists(List<Integer> list1, List<Integer> list2) {
+    public static int solution1(List<Integer> list1, List<Integer> list2) {
+        long startTime = System.nanoTime();
         int sum = 0;
         //Sort the two lists
         list1.sort(Integer::compareTo);
@@ -13,6 +17,21 @@ public class Day1 {
         for (int i = 0; i < list1.size(); i++) {
             sum += (Math.abs(list1.get(i) - list2.get(i)));
         }
+        long endTime = System.nanoTime();
+        System.out.println("Time taken: " + (endTime - startTime) / 1000000 + "ms");
         return sum;
+    }
+
+    public static long solution2(List<Integer> list1, List<Integer> list2) {
+        long startTime = System.nanoTime();
+        //We traver the array obtaining the frequency of a number from list 1 in list 2
+        //The number in list 1 is the key and the frequency is the value
+        //Frequency map of list 2
+        Map<Integer, Long> frequencyMap = list2.stream().collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+        //Calculate weighted sum
+        long result = list1.stream().mapToLong(number -> frequencyMap.getOrDefault(number, 0L) * number).sum();
+        long endTime = System.nanoTime();
+        System.out.println("Time taken: " + (endTime - startTime) / 1000000 + "ms");
+        return result;
     }
 }
