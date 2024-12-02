eaf3b0b31fea8c94b7a6e8329e698e768e16a0f5
Prince Luhanga <prince@synthesis.co.za>
2024-12-02 10:30:21 +0200

    Initial commit

---
+++ advent-of-code-2024/src/test/java/advent/of/code/_4/advent/of/code/_4/AdventOfCode2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package advent.of.code._4.advent.of.code._4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdventOfCode2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
